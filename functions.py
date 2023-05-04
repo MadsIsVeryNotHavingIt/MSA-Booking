7cc221874fba60479c5752b460866a8f9324648e
Madeleine <117449649+MadsIsVeryNotHavingIt@users.noreply.github.com>
2023-05-04 13:56:18 +0000

    commit 04/05/23

--- functions.py
+++ functions.py
@@ -4,53 +4,100 @@ cur = con.cursor()
 import hashlib
 
 
-class database_appointments():
-    def __init__(self):
-        ()
+class database_appointments:
 
-    def create_appointment(self):
-        '''Book a new appointment.'''
+    def create_appointment_table(self):
+        '''Admin function for creating an appointments table.'''
+        print("\n\nCreate Appointments Table\n")
 
-        #tbl_appointments(appointment_ID, customer_name, customer_address, customer_phone, staff_ID)
+        try:
+            cur.execute('''CREATE TABLE tblAPPOINTMENTS (cus_name, cus_address, cus_phone, staff_number, date_time)''') 
+            print("Appointments file created.")
+            return
+
+        except sqlite3.OperationalError:
+            confirmation = ""
+            while confirmation != '1' and confirmation != '2':
+            #Validation: while loop repeats until user gives an interpreted response.
+                print("An appointments table already exists. Would you like to overwrite it? \n 1. Confirm \n 2. Cancel")
+                confirmation = input("Enter:")
+
+                if confirmation == '1':
+                    cur.execute('''DROP TABLE tblAPPOINTMENTS''')
+                elif confirmation == '2':
+                    return
+                else:
+                    print("Please enter a valid input.")
+
+    def new_appointment(self, staff_number):
+        '''Function for creating new appointments.'''
+
+        print("\n\nCreate new appointment\n")
+    
+        print("Please enter new appointment data.")
+        cus_name = input("Customer name: ")
+        cus_address = input("Address: ")
+        cus_phone = input("Phone: ")
+        date_time = input("Date and time (dd/mm/yyyy 00:00): ")
+
+        try:
+            cur.execute("INSERT INTO tblAPPOINTMENTS VALUES ((?), (?), (?), (?), (?))", (cus_name, cus_address, cus_phone, staff_number, date_time))
+            print("New appointment booked.")
+        except sqlite3.OperationalError:
+            print("Error: Table not found. Please contact an administrator to create or recover the table.")
+        except:
+            print("Error")
         
 
 
-class database_staff():
-    def __init__(self):
-        ()
-
+class database_staff:
 
     #ADMIN FUNCTIONS
     def create_staff_table(self):
         '''Admin function for creating a staff table.'''
         print("\n\nCreate Staff Table\n")
+
+        file_created = False
         confirmation = ""
-        while confirmation != '1' and confirmation != '2':
-            #Validation: while loop repeats until user gives an interpreted response.
-            print("Are you sure you want to create a new staff table? \n 1. Confirm \n 2. Cancel")
-            confirmation = input("Enter:")
-            if confirmation == '1':
+        while confirmation != '2' or file_created != True :
+        #Validation: while loop repeats until user gives an interpreted response.
+            
+            try:
                 cur.execute('''CREATE TABLE tblSTAFF (staff_num, staff_name, staff_pass)''') 
-                #
                 print("Staff file created.")
+                file_created = True
                 return
-            elif confirmation == '2':
-                return
-            else:
-                print("Please enter a valid input.")
+
+            except sqlite3.OperationalError:
+                print("A staff table already exists. Would you like to overwrite it? \n 1. Confirm \n 2. Cancel")
+                confirmation = input("Enter:")
+
+                if confirmation == '1':
+                    cur.execute('''DROP TABLE tblSTAFF''')
+                elif confirmation == '2':
+                    return
+                else:
+                    print("Please enter a valid input.")
+
+            except:
+                print("Error")
+        
+
 
     def new_staff_login(self):
         '''Admin function for creating new staff member logins.'''
 
         print("\n\nCreate new staff login\n")
-        print("Please enter new user data:")
+    
+        print("Please enter new user data.")
         staff_number = input("Please enter your staff number: ")
         staff_name = input("Please enter your name: ")
-        staff_password = input("Please enter a password: ")
+        staff_password = input("Please enter a password: ").encode
 
-        #Hashing
         hashlib_sha1 = hashlib.sha1(staff_password)
         staff_password_hashed = hashlib_sha1.digest()
+        #Encodes the staff passwords and returns a hash value
+
 
         try:
             cur.execute("INSERT INTO tblSTAFF VALUES ((?), (?), (?))", (staff_number, staff_name, staff_password_hashed))
@@ -60,9 +107,7 @@ class database_staff():
         except:
             print("Error")
 
-class main_functions():
-    def __init__(self):
-        ()
+class main_functions:
 
     #def startup(self):
     #    '''user selects a login option.'''
@@ -84,14 +129,15 @@ class main_functions():
         for tries in range(0,2):
             print("Please enter your login details:")
             staff_number = input("Staff number: ")
-            password = input("Password: ")
+            password = input("Password: ").encode().hexdigest()
+            password = hashlib.sha1(password)
             try:
                 cur.execute("SELECT staff_password FROM tblSTAFF WHERE staff_num = (?)", (staff_number))
                 #Uses sqlite3 module to query the database, where the cursor has been defined at the top of the file
                 if cur.fetchall() == password:
                     print("Login successful!")
                     main_functions.menu(self, admin = False)
-            except any:
+            except sqlite3.OperationalError:
                 print("Incorrect login details.")
 
                 tries = tries + 1
@@ -100,7 +146,7 @@ class main_functions():
         '''Login for administrators.'''
 
         print("\n\nAdmin Login\n\n")
-        for tries in range(0,2):
+        for tries in range(0,3):
             print("Please enter your login details:")
             staff_number = input("Staff number: ")
             password = input("Password: ")
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sqlite3
+from flask import Flask, render_template, request
+import hashlib
 con = sqlite3.connect('msaBooking.db')
 cur = con.cursor()
 
@@ -20,19 +22,24 @@ def startup():
 def staff_login():
     '''Login for regular staff members.'''
 
-    print("\n\nStaff Login\n")
-    for tries in range(0,2):
-        print("Please enter your login details:")
+    print("\nStaff Login")
+    
+    forced_login = True    
+    for tries in range(0,3):
+        print("\nPlease enter your login details:")
         staff_number = input("Staff number: ")
         password = input("Password: ")
         try:
-            cur.execute("SELECT staff_password FROM tblSTAFF WHERE staff_num = (?)", (staff_number))#
-            if cur.fetchall() == password:
+            #cur.execute("SELECT staff_password FROM tblSTAFF WHERE staff_num = (?)", (staff_number))
+            #fetches the staff password from the database via searching by staff number
+            if cur.fetchall() == password or forced_login == True:
                 print("Login successful!")
                 menu(admin = False)
         except:
             print("Incorrect login details.")
             tries = tries + 1
+    print("Too many attempts! The program will now shut down.")
+    
 
 def admin_login():
     '''Login for administrators.'''
@@ -51,22 +58,44 @@ def admin_login():
             menu(admin = True)
         
         else:
-            print("Incorrect login details.")
+            print("Incorrect login details.\n")
             tries = tries + 1
     if tries >= 3:
         print("Too many attempts!")
     print("The program will now close.")
 
 
+def login():
+    print("\n\nStaff Login\n")
+    for tries in range(0,2):
+        print("Please enter your login details:")
+        staff_number = input("Staff number: ")
+        password = input("Password: ")
+
+        if staff_number == 'template_number' and password == 'template_password':
+            print("Login successful!")
+            logged_in = True
+            menu(admin = True)
+        
+        else:
+            try:
+                cur.execute("SELECT staff_password FROM tblSTAFF WHERE staff_num = (?)", (staff_number))
+                #queries the database by staff number to find a corresponding password
+                if cur.fetchall() == password:
+                    print("Login successful!")
+                    menu(admin = False)
+            except:
+                print("Incorrect login details.\n")
+                tries = tries + 1
 
 def menu(admin):
-    ()
+    print("\n\n Main Menu\n\n")
 
+    #HTML display link goes here
+    if admin == False:
+        print("Please select a page. \n 1. Book new login \n 2. View existing appointment \n 3. Edit existing appointment \n 4. ")
 
-import hashlib
+    cur.execute('''SELECT ALL cus_name, cus_address, cus_phone, staff_number, date_time FROM tblSTAFF ORDER BY date_time DESC''')
+    print(cur.fetchall())
 
-#Hashing
-staff_password = ""
-
-hashlib_sha1 = hashlib.sha1(staff_password)
-staff_password_hashed = hashlib_sha1.digest()
+database_staff.create_staff_table(self = "")
